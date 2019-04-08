deff7214b1ac4e77b2b78062622f85113337f598
Game Shared <GameDesignShared@mac-lab-d25tq04ygg7d.local>
2019-04-08 14:25:54 -0500

    jumping + physics

--- Hook Knight/Hook Knight/GameScene.swift
+++ Hook Knight/Hook Knight/GameScene.swift
@@ -15,10 +15,10 @@ struct physTypes
     static let None:UInt32 =    0b00000000
     static let Player:UInt32 =    0b00000001
     static let Ground:UInt32 =    0b00000010
-}
+}// phystypes
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var gameStart:Bool = true
     
@@ -29,16 +29,28 @@ class GameScene: SKScene {
     var leftPressed:Bool=false
     
     var blockPlacement:Int=0
+    
+    var lastJump=NSDate()
+    
     var player=HookKnightClass()
     
     override func didMove(to view: SKView) {
         
+        physicsWorld.contactDelegate = self as SKPhysicsContactDelegate
+        player.sprite.physicsBody=SKPhysicsBody(rectangleOf:player.sprite.size)
+        player.sprite.physicsBody!.categoryBitMask=physTypes.Player
+        player.sprite.physicsBody!.allowsRotation=false
+        player.sprite.physicsBody!.affectedByGravity=true
+        player.sprite.physicsBody!.isDynamic=true
+        
+        
+        
         addChild(player.sprite)
-       
+       player.sprite.name="player"
         
         makeLevel()
         
-    }
+    }// didmove
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -71,7 +83,7 @@ class GameScene: SKScene {
         case 15:
            
             makeLevel()
-        case 13:
+        case 49:
             upPressed=true
             
         case 1:
@@ -89,14 +101,14 @@ class GameScene: SKScene {
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
        
-    }
+    }// keydown
     
     
     override func keyUp(with event: NSEvent) {
         switch event.keyCode {
             
         
-        case 13:
+        case 49:
             upPressed=false
             
         case 1:
@@ -114,7 +126,7 @@ class GameScene: SKScene {
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
         
-    }
+    }// key up
     
     
     
@@ -128,8 +140,8 @@ class GameScene: SKScene {
                 {
                     node.removeFromParent()
                 }
-            }
-        }
+            }// if node isnt nil
+        }// for node in self
         
             var height:Int=0
             var hole:Bool=false
@@ -158,7 +170,7 @@ class GameScene: SKScene {
                     height=blockPlacement
                     player.sprite.position.y = CGFloat(height)*64+32-size.height/4
                     player.sprite.position.x = -size.width/2.2 + 25
-                }
+                }// if platform = 0
                 
                 if height < 0
                 {
@@ -173,6 +185,11 @@ class GameScene: SKScene {
                     let block=SKSpriteNode(imageNamed: "platformA")
                     block.position=gridOut(x:platform, y: height)
                     addChild(block)
+                    block.physicsBody=SKPhysicsBody(rectangleOf: block.size)
+                    block.physicsBody!.categoryBitMask=physTypes.Ground
+                    block.physicsBody!.isDynamic=false
+                    block.physicsBody!.affectedByGravity=false
+                    block.physicsBody!.allowsRotation=false
                     block.name="block"
                 }
                 else
@@ -194,25 +211,35 @@ class GameScene: SKScene {
     
     func checkKeys()
     {
-        if upPressed==true
-        {
-            player.sprite.position.y+=10
-        }
+       
+    
         
-        if downPressed==true
+        if upPressed==true && -lastJump.timeIntervalSinceNow > 2.0
         {
-            player.sprite.position.y-=10
+            
+            player.sprite.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 10))
+            lastJump = NSDate()
         }
-        if rightPressed==true
-        {
-            player.sprite.position.x+=10
-        }
-        if leftPressed==true
-        {
-            player.sprite.position.x-=10
-        }
-        
+            else
+            {
+                upPressed=false
+            }
+            
+            if downPressed==true
+            {
+                player.sprite.position.y-=10
+            }
+            if rightPressed==true
+            {
+                player.sprite.position.x+=10
+            }
+            if leftPressed==true
+            {
+                player.sprite.position.x-=10
+            }
     }
+        
+    
     
     func checkBoundaries()
     {
@@ -228,6 +255,7 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
         checkKeys()
         checkBoundaries()
     }
